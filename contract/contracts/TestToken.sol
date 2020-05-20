c38757bba5937ac4c32a3527ecdd56f8ced53c12
Hui Min HE <joyioushe@gmail.com>
2020-05-20 15:41:16 +0800

    Use truffle coverage in CI

--- contract/contracts/TestToken.sol
+++ contract/contracts/TestToken.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.21 <0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
 
 contract TestToken is ERC20 {
